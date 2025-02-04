82a72e8e9ff6adb092a8bdc22ecc2aa68fe7572e
jasvix12 <jasvialvarez@gmail.com>
2025-02-04 13:50:47 -0500

    exitoso

--- lib/src/pages/login_screen.dart
+++ lib/src/pages/login_screen.dart
@@ -4,48 +4,85 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:firebase_auth/firebase_auth.dart'; // Importa Firebase Auth
 import 'acept-permisos.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen>
+    with SingleTickerProviderStateMixin {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  late AnimationController _controller;
+  late Animation<double> _positionAnimation;
+  bool _showSuccessMessage = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+    );
+
+    // Animación de posición con efecto de rebote
+    _positionAnimation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOutBack, // Efecto de rebote sutil
+    );
+  }
+
   Future<void> signInWithGoogle(BuildContext context) async {
     try {
-      // Inicia sesión con Google
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
 
       if (googleUser == null) {
-        // Si el usuario cancela el inicio de sesión
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(content: Text('Inicio de sesión cancelado')),
         );
         return;
       }
 
-      // Obtiene las credenciales de Google
-      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
 
-      // Crea las credenciales de Firebase usando las credenciales de Google
       final AuthCredential credential = GoogleAuthProvider.credential(
         accessToken: googleAuth.accessToken,
         idToken: googleAuth.idToken,
       );
 
-      // Inicia sesión con Firebase usando las credenciales
-      final UserCredential userCredential = await _auth.signInWithCredential(credential);
+      final UserCredential userCredential =
+          await _auth.signInWithCredential(credential);
 
-      // Si el inicio de sesión es exitoso, navega a la siguiente pantalla
       if (userCredential.user != null) {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => AceptPermisosScreen()),
-        );
+        setState(() {
+          _showSuccessMessage = true; // Mostrar el mensaje de éxito
+        });
+
+        // Iniciar la animación
+        _controller.forward();
+
+        // Ocultar el mensaje después de 2 segundos
+        Future.delayed(const Duration(seconds: 2), () {
+          setState(() {
+            _showSuccessMessage = false;
+          });
+          // Reiniciar el controlador de animación
+          _controller.reset();
+
+          // Navegar a la siguiente pantalla
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => AceptPermisosScreen()),
+          );
+        });
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(content: Text('Error al iniciar sesión')),
         );
       }
     } catch (error) {
-      // Si ocurre algún error
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Error durante el inicio de sesión: $error')),
       );
@@ -53,42 +90,94 @@ class LoginScreen extends StatelessWidget {
   }
 
   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(184, 197, 34, 1),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/perfil.png',
-              width: 150,
-              height: 150,
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/perfil.png',
+                  width: 150,
+                  height: 150,
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.black,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 15),
+                  ),
+                  icon: const FaIcon(
+                    FontAwesomeIcons.google, // Icono de Google de FontAwesome
+                    size: 30.0, // Tamaño del icono
+                    color: Colors.red, // Color del icono (rojo como Google)
+                  ),
+                  label: const Text(
+                    'Iniciar sesión con Google',
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  onPressed: () {
+                    signInWithGoogle(context);
+                  },
+                ),
+              ],
             ),
-            const SizedBox(height: 20),
-            ElevatedButton.icon(
-          style: ElevatedButton.styleFrom(
-    backgroundColor: Colors.white,
-    foregroundColor: Colors.black,
-    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-  ),
-  icon: const FaIcon(
-    FontAwesomeIcons.google, // Icono de Google de FontAwesome
-    size: 30.0, // Tamaño del icono
-    color: Colors.red, // Color del icono (rojo como Google)
-  ),
-  label: const Text(
-    'Iniciar sesión con Google',
-    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-  ),
-  onPressed: () {
-    signInWithGoogle(context);
+          ),
+          // Animación del cuadro verde
+          if (_showSuccessMessage)
+            AnimatedBuilder(
+              animation: _positionAnimation,
+              builder: (context, child) {
+                return Positioned(
+                  top: -100 + _positionAnimation.value * 150, // Animación de caída
+                  left: MediaQuery.of(context).size.width * 0.1,
+                  right: MediaQuery.of(context).size.width * 0.1,
+                  child: child!,
+                );
               },
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Icon(Icons.check_circle, color: Colors.white),
+                    const SizedBox(width: 10),
+                    Expanded( // Permite ajustar el texto al espacio disponible
+                      child: Text(
+                        "Éxito\nInicio de sesión exitoso",
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                        textAlign: TextAlign.left, // Alinear texto a la izquierda
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ],
-        ),
+        ],
       ),
     );
   }
 }
+
+
 
